f117af7c670f18cbc46fc4d2f5e957e08aec6855
alaaMagdy <alaa_magdy@outlook.com>
2025-12-22 01:10:13 +0400

    make the database cool and test db connection

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import app from "./app";
+import { config } from "dotenv";
+import { connectDatabase } from "./mysql/connection";
+
+config();
+
+const init = async () => {
+  try {
+    await connectDatabase();
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log("now app is listing on this port =", PORT);
+    });
+  } catch (error) {
+    console.log("error from index.ts =", error);
+    process.exit(1);
+  }
+};
+
+init();
---
+++ backend/src/mysql/connection.ts
@@ -0,0 +1,26 @@
+import { createPool, Pool } from "mysql2/promise";
+
+let pool: Pool;
+
+const connectDatabase = async () => {
+  try {
+    pool = createPool({
+      port: +process!.env!.MYSQL_PORT!,
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      //   waitForConnections: true,
+      //   connectionLimit: 10,
+      //   queueLimit: 0,
+    });
+
+    // Optional: test connection (recommended for beginners)
+    await pool.getConnection();
+    console.log("✅ MySQL connected");
+  } catch (err) {
+    console.error("❌ MySQL connection failed:", err);
+    throw err;
+  }
+};
+
+export { connectDatabase, Pool };
